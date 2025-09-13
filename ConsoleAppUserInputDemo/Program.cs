ea86f3202e92aa936b81d865212cdfda34d53e0b
Daniel Aleriwa O. <dannytech25@gmail.com>
2025-09-13 17:49:14 +0100

    Add project files.

---
+++ ConsoleAppUserInputDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*
+ this code is to collect user input
+ */
+
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+Console.WriteLine("Input your name ");
+name = Console.ReadLine();
+
+Console.WriteLine("What is your age: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+int workingYearsRemaining = retirementAge - age;
+Console.WriteLine($"Your name is {name} and your Working year remainingi is {workingYearsRemaining}");
